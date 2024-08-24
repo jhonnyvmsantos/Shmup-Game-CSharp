13e90e518a3f4c7b4752c195c1740388f97fe87e
DioneKx <mjhonnyvieira@gmail.com>
2024-08-24 19:43:58 -0300

    BackGround-Initial

---
+++ Shmup Game/shmup_game/MainForm.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mjhon
+ * Date: 24/08/2024
+ * Time: 18:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace shmup_game
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			this.FormBorderStyle = FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false; this.BackColor = Color.Black;
+		}
+	}
+}
